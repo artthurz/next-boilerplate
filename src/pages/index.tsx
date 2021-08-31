f471745aaeac254283f7f8774d4f5cb3f9aa2f4b
Arthur <arthur.hauenstein@universo.univates.br>
2021-08-31 11:51:15 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import * as S from "@/styles/pages/Home";
+
+const Home: NextPage = () => {
+  return (
+    <S.Container>
+      <Head>
+        <title>Next Boilerplate</title>
+      </Head>
+      <h1>Olá</h1>
+    </S.Container>
+  );
+}
+
+export default Home
+
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  padding-bottom: 20px;
+
+  h2 {
+    font-size: 24px;
+    color: white;
+    margin-top: 10px;
+    text-align: center;
+  }
+  p {
+    font-size: 48px;
+    color: white;
+    margin-top: 10px;
+  }
+`;
